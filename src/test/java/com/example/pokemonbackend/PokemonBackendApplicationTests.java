b7d80719e9879225f071a6312f4b89ae2d5f5da1
Rasmus Kibshede <2740ralle@gmail.com>
2022-03-11 22:13:23 +0100

    test git actions

--- src/test/java/com/example/pokemonbackend/PokemonBackendApplicationTests.java
+++ src/test/java/com/example/pokemonbackend/PokemonBackendApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.pokemonbackend;
 
+import com.example.pokemonbackend.model.Pokemon;
+import com.example.pokemonbackend.repository.PokemonRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class PokemonBackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
+  @Test
+  void testFunc() {
+
+
+    Assertions.assertEquals(true, true);
+  }
 
 }
