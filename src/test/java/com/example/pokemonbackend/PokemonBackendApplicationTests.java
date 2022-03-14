f89279a84696f1afb64cae4207ec537cd310a7b2
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-03-14 16:01:10 +0100

    test git actions

--- src/test/java/com/example/pokemonbackend/PokemonBackendApplicationTests.java
+++ src/test/java/com/example/pokemonbackend/PokemonBackendApplicationTests.java
@@ -1,16 +1,11 @@
 package com.example.pokemonbackend;
 
-import com.example.pokemonbackend.model.Pokemon;
-import com.example.pokemonbackend.repository.PokemonRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
 @SpringBootTest
 class PokemonBackendApplicationTests {
-
 
   @Test
   void testFunc() {
